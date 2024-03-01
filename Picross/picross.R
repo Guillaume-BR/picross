2ebe96281aaa5715bd6a72523905cceef70becf0
e20190017420 <maxence.lamure@etu.umontpellier.fr>
2024-03-01 11:15:53 +0100

    ajout commentaires

--- Picross/picross.R
+++ Picross/picross.R
@@ -6,8 +6,8 @@ ui <- fluidPage(
   shinyjs::useShinyjs(),
   mainPanel(
     fluidRow(
-      column(12, align = "center", sliderInput("grid_size", label = "Taille de la grille", value = 5, min = 5, max = 20)),
-      column(10, uiOutput("grid_container"))
+      column(12, align = "center", sliderInput("grid_size", label = "Taille de la grille", value = 5, min = 5, max = 20)), # Choix de la taille de la grille
+      column(10, uiOutput("grid_container")) # Grille totale
     )
   )
 )
@@ -20,43 +20,44 @@ server <- function(input, output, session) {
   decompte_colonne <- reactiveVal(NULL)
   
   observeEvent(input$grid_size, {
-    # Définir la taille de la matrice totale
-    g_size <- input$grid_size
-    ajout <- ceiling(input$grid_size/2)
-    size <- g_size+ajout
+    # Définir les tailles de grille
+    grid_size <- input$grid_size # Taille de la grille de jeu choisie
+    ajout <- ceiling(input$grid_size/2) # Taille supplémentaire pour les indices
+    size <- grid_size+ajout # Taille de la grille totale
     
-    # Générer la matrice avec des valeurs aléatoires uniquement pour la grille de jeu
+    # Générer la matrice random_numbers_val avec des valeurs aléatoires uniquement pour la grille de jeu
     random_numbers_val <- matrix(0, nrow = size, ncol = size)
-
-    #définir difficulté ici prob=(1-p,p)
-    random_numbers_val[ajout+1:g_size, ajout+1:g_size] <- sample(c(0,1), prob = c(0.5, 0.5), g_size^2, replace = TRUE)
+    # Définir difficulté ici prob=(1-p,p)
+    random_numbers_val[ajout+1:grid_size, ajout+1:grid_size] <- sample(c(0,1), prob = c(0.3, 0.7), grid_size^2, replace = TRUE)
     random_numbers(random_numbers_val)
     
-    # Exclure les 5 premières lignes et colonnes
+    # Exclure les lignes et colonnes prévues pour les indices
     num <- random_numbers_val[-c(1:ajout), -c(1:ajout)]
     numt <- t(num)
     #print(num)
     
-    # DECOMPTE PAR LIGNE
-    # Initialiser le vecteur de compteurs
-    counters_l <- matrix(0, nrow = g_size, ncol = ajout)
-    counters_c <- matrix(0, nrow = g_size, ncol = ajout)
+    # DECOMPTE DES INDICES
+    # Initialiser les matrices des indices
+    counters_l <- matrix(0, nrow = grid_size, ncol = ajout)
+    counters_c <- matrix(0, nrow = grid_size, ncol = ajout) # Sera transposée par la suite
     
-    for (i in 1:g_size) {
+    for (i in 1:grid_size) {
       c <- 1
       l <- 1
-      counterc <- 0  # Réinitialiser le compteur pour chaque ligne
+      # (Ré)initialiser le compteur pour chaque ligne
+      counterc <- 0
       counterl <- 0
-      for (j in 1:g_size) {
-        if (num[i, j] == 1) { # Incrémenter pour case noire consécutive
+      for (j in 1:grid_size) { # Décompte par ligne
+        if (num[i, j] == 1) { # Incrémenter pour chaque case noire (1) consécutive
           counterc <- counterc + 1
-        } else { # Attribuer et réinitialiser le compteur si on tombe sur une case blanche
+        } else { # Attribuer et réinitialiser le compteur si on tombe sur une case blanche (0)
           if (counterc > 0) {
             counters_l[i, c] <- counterc
             c <- c + 1
             counterc <- 0
           }
-        } # même principe
+        } 
+        # Même principe par colonne
         if (numt[i, j] == 1) {
           counterl <- counterl + 1
         } else {
@@ -71,42 +72,41 @@ server <- function(input, output, session) {
       counters_l[i, c] <- counterc
       counters_c[i, l] <- counterl
       
-      liste = counters_c[i,]
-      zeros <- length(liste[liste == 0])
-      zeros <- c(rep("", zeros))
-      non_zeros <- liste[liste != 0]
-      counters_c[i,] <- c(zeros, non_zeros)
+      # Inverser les zéros remplacés par une chaîne de caractères vide avec les indices
+      counters_c[i,] <- c(c(rep("", length(counters_c[i,][counters_c[i,] == 0]))), counters_c[i,][counters_c[i,] != 0])
+      counters_l[i,] <- c(c(rep("", length(counters_l[i,][counters_l[i,] == 0]))), counters_l[i,][counters_l[i,] != 0])
 
-      liste = counters_l[i,]
-      zeros <- length(liste[liste == 0])
-      zeros <- c(rep("", zeros))
-      non_zeros <- liste[liste != 0]
-      counters_l[i,] <- c(zeros, non_zeros)
+      # Equivalent plus compréhensible
+      #liste = counters_l[i,]
+      #zeros <- length(liste[liste == 0])
+      #zeros <- c(rep("", zeros))
+      #non_zeros <- liste[liste != 0]
+      #counters_l[i,] <- c(zeros, non_zeros)
     }
 
+    # Transposition de la matrice des indices des colonnes
     counters_ct <- t(counters_c)
     
     # AFFICHAGE GRILLE
     output$grid_container <- renderUI({
       grid_divs <- lapply(1:size, function(i) {
         lapply(1:size, function(j) {
+          # Suppression des bordures hors de la grille de jeu et centrage des indices
           common_style <- paste("text-align: center; font-weight: bold; margin: 1px;", ifelse(i <= ajout | j <= ajout, "border: none;", "border: 1px solid black;"))
           
-          if (i > ajout & j <= ajout) { # indices lignes
+          if (i > ajout & j <= ajout) { # Affichage des indices lignes
             div(
-              class = "cell",
               style = common_style,
               counters_l[i-ajout, j]
             )
-          } else if (i <= ajout & j > ajout) { # indices colonnes
+          } else if (i <= ajout & j > ajout) { # Affichage des indices colonnes
             div(
-              class = "cell",
               style = common_style,
               counters_ct[i, j-ajout]
             )
-          } else { #sous-grille de jeu
+          } else { # Affichage de la sous-grille de jeu
             div(
-              class = "grid_cell",
+              class = "grid_cell", # Classe utilisée pour détecter les clics par la suite
               style = common_style,
               id = paste("cell_", i, "_", j)
             )
@@ -114,13 +114,13 @@ server <- function(input, output, session) {
         })
       })
       
-      tags$div( # grille totale
-        id = "grid",
+      tags$div( # Affichage de la grille totale
         style = paste("width: 500px; height: 500px; display: grid; grid-template-columns: repeat(", size, ", 1fr); grid-template-rows: repeat(", size, ", 1fr);"),
         grid_divs
       )
     })
     
+    # Détection clic
     shinyjs::runjs('
   $(document).on("click", ".grid_cell", function(){
     var cell = $(this);
