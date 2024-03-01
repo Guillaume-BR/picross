1b671b2069cf8e85329004ce1116fe333be13e67
e20190017420 <maxence.lamure@etu.umontpellier.fr>
2024-03-01 10:34:27 +0100

    optimisation indices avec Guillaume

--- Picross/picross.R
+++ Picross/picross.R
@@ -20,93 +20,103 @@ server <- function(input, output, session) {
   decompte_colonne <- reactiveVal(NULL)
   
   observeEvent(input$grid_size, {
-    # Définir la taille de la matrice en ajoutant 5 à grid_size
-    # Attention il faut potentiellement grid_size/2 info
-    size <- input$grid_size + 5
+    # Définir la taille de la matrice totale
+    g_size <- input$grid_size
+    ajout <- ceiling(input$grid_size/2)
+    size <- g_size+ajout
     
-    # Générer la matrice avec des valeurs aléatoires uniquement pour les cases qui ne sont pas parmi les 5 premières lignes et 5 premières colonnes
+    # Générer la matrice avec des valeurs aléatoires uniquement pour la grille de jeu
     random_numbers_val <- matrix(0, nrow = size, ncol = size)
+
     #définir difficulté ici prob=(1-p,p)
-    random_numbers_val[6:size, 6:size] <- sample(c(0,1), prob = c(0.3, 0.7),(size - 5)^2, replace = TRUE)
+    random_numbers_val[ajout+1:g_size, ajout+1:g_size] <- sample(c(0,1), prob = c(0.5, 0.5), g_size^2, replace = TRUE)
     random_numbers(random_numbers_val)
     
     # Exclure les 5 premières lignes et colonnes
-    num <- random_numbers_val[-c(1:5), -c(1:5)]
+    num <- random_numbers_val[-c(1:ajout), -c(1:ajout)]
+    numt <- t(num)
+    #print(num)
     
-    #DECOMPTE PAR LIGNE
-    # EN commenant par la fin de la ligne, on aurait pas les 0
+    # DECOMPTE PAR LIGNE
     # Initialiser le vecteur de compteurs
-    counters_l <- matrix(0, nrow = size - 5, ncol = size - 5)
+    counters_l <- matrix(0, nrow = g_size, ncol = ajout)
+    counters_c <- matrix(0, nrow = g_size, ncol = ajout)
     
-    for (i in 1:(size - 5)) {
+    for (i in 1:g_size) {
       c <- 1
-      counter <- 0  # Réinitialiser le compteur pour chaque ligne
-      for (j in 1:(size - 5)) {
-        if (num[i, j] == 1) { # Incrémenter pour case noire consécutive
-          counter <- counter + 1
-        } else { # Attribuer et réinitialiser le compteur si on tombe sur une case blanche
-          if (counter > 0) {
-            counters_l[i, c] <- counter
-            c <- c + 1
-            counter <- 0
-          }
-        }
-      }
-      counters_l[i, c] <- counter # Attribuer le compteur en passant à une nouvelle ligne
-    }
-    
-    #DECOMPTE PAR COLONNE (même principe en inversant les indices)
-    # En créant une fonction sur une matrice on pourrait l'appliquer à la transposée : plus simple peut-être
-    counters_c <- matrix(0, nrow = size - 5, ncol = size - 5)
-    
-    for (j in 1:(size - 5)) {
       l <- 1
-      counter <- 0
-      for (i in 1:(size - 5)) {
-        if (num[i, j] == 1) {
-          counter <- counter + 1
+      counterc <- 0  # Réinitialiser le compteur pour chaque ligne
+      counterl <- 0
+      for (j in 1:g_size) {
+        if (num[i, j] == 1) { # Incrémenter pour case noire consécutive
+          counterc <- counterc + 1
+        } else { # Attribuer et réinitialiser le compteur si on tombe sur une case blanche
+          if (counterc > 0) {
+            counters_l[i, c] <- counterc
+            c <- c + 1
+            counterc <- 0
+          }
+        } # même principe
+        if (numt[i, j] == 1) {
+          counterl <- counterl + 1
         } else {
-          if (counter > 0) {
-            counters_c[l, j] <- counter
+          if (counterl > 0) {
+            counters_c[i, l] <- counterl
             l <- l + 1
-            counter <- 0
+            counterl <- 0
           }
         }
       }
-      counters_c[l, j] <- counter
+      # Attribuer le compteur en passant à une nouvelle ligne
+      counters_l[i, c] <- counterc
+      counters_c[i, l] <- counterl
+      
+      liste = counters_c[i,]
+      zeros <- length(liste[liste == 0])
+      zeros <- c(rep("", zeros))
+      non_zeros <- liste[liste != 0]
+      counters_c[i,] <- c(zeros, non_zeros)
+
+      liste = counters_l[i,]
+      zeros <- length(liste[liste == 0])
+      zeros <- c(rep("", zeros))
+      non_zeros <- liste[liste != 0]
+      counters_l[i,] <- c(zeros, non_zeros)
     }
+
+    counters_ct <- t(counters_c)
     
     # AFFICHAGE GRILLE
     output$grid_container <- renderUI({
       grid_divs <- lapply(1:size, function(i) {
         lapply(1:size, function(j) {
-          common_style <- paste("margin: 1px;", ifelse(i <= 5 | j <= 5, "border: none;", "border: 1px solid black;"))
+          common_style <- paste("text-align: center; font-weight: bold; margin: 1px;", ifelse(i <= ajout | j <= ajout, "border: none;", "border: 1px solid black;"))
           
-          if (i >= 5 & j <= 5) {
+          if (i > ajout & j <= ajout) { # indices lignes
             div(
               class = "cell",
-              style = paste("text-align: center; font-weight: bold; border: none;", common_style),
-              counters_l[i-5, j]
+              style = common_style,
+              counters_l[i-ajout, j]
             )
-          } else if (i <= 5 & j >= 5) {
+          } else if (i <= ajout & j > ajout) { # indices colonnes
             div(
               class = "cell",
-              style = paste("text-align: center; font-weight: bold; border: none;", common_style),
-              counters_c[i, j-5]
+              style = common_style,
+              counters_ct[i, j-ajout]
             )
-          } else {
+          } else { #sous-grille de jeu
             div(
               class = "grid_cell",
-              style = paste("margin: 1px;", ifelse(i <= 5 | j <= 5, "border: none;", "border: 1px solid black;")),
+              style = common_style,
               id = paste("cell_", i, "_", j)
             )
           }
         })
       })
       
-      tags$div(
+      tags$div( # grille totale
         id = "grid",
-        style = paste("width: 500px; height: 500px; border: 1px solid black; display: grid; grid-template-columns: repeat(", size, ", 1fr); grid-template-rows: repeat(", size, ", 1fr);"),
+        style = paste("width: 500px; height: 500px; display: grid; grid-template-columns: repeat(", size, ", 1fr); grid-template-rows: repeat(", size, ", 1fr);"),
         grid_divs
       )
     })
@@ -122,7 +132,6 @@ server <- function(input, output, session) {
       cell.css("background-color", "red");
     } else {
       cell.css("background-color", "white");
-      cell.html(""); // Supprime le contenu de la cellule
     }
   });
 ')
