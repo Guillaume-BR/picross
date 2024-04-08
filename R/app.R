303d5898b4f5747afd73380de506ef3dace3a2e8
Guillaume-BR <guillaume.bernardreymond@gmail.com>
2024-04-08 21:02:52 +0200

    enleve_verif

--- R/app.R
+++ R/app.R
@@ -33,7 +33,20 @@ update_player_matrix <- function(i, j) {
 }
 
 # Fonction pour vérifier les positions des "1" dans les matrices
-source("verif_pos.R")
+#' Vérifie la position de 1 dans deux matrices
+#' 
+#' @param matrice1 : première matrice
+#' @param matrice2 : deuxième matrice
+#' @returns TRUE or FALSE
+#' @examples
+#' verifier_positions(matrix(c(1,0,1,1), nrow = 2 , ncol=2),matrix(c(1,0,1,1), nrow = 2 , ncol=2))
+#' verifier_positions(matrix(c(1,0,1,1,1,0), nrow = 2 , ncol=3),matrix(c(1,0,1,0,1,0), nrow = 2 , ncol=3))
+#' @export
+verifier_positions <- function(matrice1, matrice2) {
+  positions_matrice1 <- which(matrice1 == 1, arr.ind = TRUE)
+  positions_matrice2 <- which(matrice2 == 1, arr.ind = TRUE)
+  return(identical(positions_matrice1, positions_matrice2))
+}
 
 # Définir les futures matrices réactives
 joueur <- reactiveVal(matrix(0, nrow = 1, ncol = 1))
